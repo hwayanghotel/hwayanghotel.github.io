cbe34b11511e750b279c9493267f3ff8deeb5427
holywaterBetter <holywater.better@gmail.com>
2023-05-28 22:59:15 +0900

    feat(#6): 리뷰사진 자동으로 움직이게 수정

--- src/app/home/home-review/home-review.component.ts
+++ src/app/home/home-review/home-review.component.ts
@@ -15,6 +15,12 @@ export class HomeReviewComponent {
   private _changed: boolean = false;
   private _timeoutHandle: any;
 
+  constructor() {
+    setInterval(() => {
+      this.onClickDot((this.currentIndex + 1) % ReviewList.length);
+    }, 3000);
+  }
+
   onScroll() {
     if (!this._changed) {
       this._changed = true;
@@ -51,7 +57,7 @@ const ReviewList = [
     selected: true,
     href: 'https://blog.naver.com/sam_box/222827110393',
     img: 'assets/home-review/sam_box_preview.jpeg',
-    date: '2022.07.24 방문',
+    date: '2022.07.24 방문 리뷰',
     tag: '#펜션 #평상 #백숙',
     description: '물놀이 하기 최고!',
   },
@@ -59,7 +65,7 @@ const ReviewList = [
     selected: false,
     href: 'https://blog.naver.com/vhfpqj327/222791258320',
     img: 'assets/home-review/vhfpqj327_preview.jpg',
-    date: '2022.06.29 방문',
+    date: '2022.06.29 방문 리뷰',
     tag: '#펜션 #평상 #백숙',
     description: '완벽했던 하루❤',
   },
@@ -67,7 +73,7 @@ const ReviewList = [
     selected: false,
     href: 'https://blog.naver.com/plask79/222819401741',
     img: 'assets/home-review/plask79_preview.jpg',
-    date: '2022.07.19 방문',
+    date: '2022.07.19 방문 리뷰',
     tag: '#백숙 #물놀이',
     description: '그냥 풍경만 봐도 피서입니다',
   },
@@ -75,7 +81,7 @@ const ReviewList = [
     selected: false,
     href: 'https://blog.naver.com/ppangu0113/222806544272',
     img: 'assets/home-review/ppangu0113_preview.jpg',
-    date: '2022.07.10 방문',
+    date: '2022.07.10 방문 리뷰',
     tag: '#백숙 #물놀이',
     description: '엄청 친절하시고 음식 맛도 좋아요',
   },
@@ -83,7 +89,7 @@ const ReviewList = [
     selected: false,
     href: 'https://blog.naver.com/yun_0225/222440823005',
     img: 'assets/home-review/yun_0225_preview.jpg',
-    date: '2022.06.29 방문',
+    date: '2022.06.29 방문 리뷰',
     tag: '#펜션 #평상 #백숙',
     description: '계곡 진짜 10초 컷',
   },
