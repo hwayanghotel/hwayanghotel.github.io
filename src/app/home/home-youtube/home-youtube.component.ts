769a871578059e01b7c7391645e6bc529bdf8827
holywaterBetter <holywater.better@gmail.com>
2023-05-29 11:10:07 +0900

    feat(#6): HomeYoutube 비율 16:9 설정

--- src/app/home/home-youtube/home-youtube.component.ts
+++ src/app/home/home-youtube/home-youtube.component.ts
@@ -7,4 +7,19 @@ import { Component, Input } from '@angular/core';
 })
 export class HomeYoutubeComponent {
   @Input() code: string = 'rBiuJ_Usg10';
+
+  get width(): number {
+    const windowWidth = window.innerWidth;
+    if (windowWidth > 1000) {
+      return windowWidth * 0.4;
+    } else if (windowWidth > 640) {
+      return windowWidth * 0.6;
+    } else {
+      return windowWidth * 0.8;
+    }
+  }
+
+  get height(): number {
+    return (this.width / 16) * 9;
+  }
 }
