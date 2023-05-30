87e9fa85b796a59bbd346481b85132067f37669b
holywaterBetter <holywater.better@gmail.com>
2023-05-30 23:40:58 +0900

    feat(#4): fragment(#) URL을 permalink로 수정

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,7 +18,7 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      useHash: true,
+      useHash: false,
       scrollPositionRestoration: 'top',
     }),
   ],
