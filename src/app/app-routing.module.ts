16d5fb318f2d18da7f660d3d89ac1ce35a877e50
holywaterBetter <holywater.better@gmail.com>
2023-05-30 23:55:44 +0900

    feat(#4): fragment(#) URL로 원복

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,7 +18,7 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      useHash: false,
+      useHash: true,
       scrollPositionRestoration: 'top',
     }),
   ],
