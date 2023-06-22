2cdf7ce7d775c26f829a6d159d896016b7b92f64
holywaterBetter <holywater.better@gmail.com>
2023-06-22 16:37:48 +0900

    feat(#9): Price 객체 추가 및 반영, submodule update

--- src/app/pension-service/food/food.component.ts
+++ src/app/pension-service/food/food.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { Price } from 'src/assets/price';
 
 @Component({
   selector: 'food',
   templateUrl: './food.component.html',
   styleUrls: ['./food.component.scss', '../pension-service.scss'],
 })
-export class FoodComponent {}
+export class FoodComponent {
+  Price = Price;
+}
