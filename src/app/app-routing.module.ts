3eda486d3b9b0e2821c006d98adce09fbf9f8377
holywaterBetter <holywater.better@gmail.com>
2023-07-15 11:22:57 +0900

    One page 개발

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,25 +7,27 @@ import { StoryComponent } from './story/story.component';
 import { RoomComponent } from './pension-service/room/room.component';
 import { ManagerComponent } from 'manager/manager/manager.component';
 import { ReservationComponent } from 'reservation/reservation/reservation.component';
+import { LinkTreeComponent } from './link-tree/link-tree.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'food', component: FoodComponent },
-  { path: 'flat-bench', component: FlatBenchComponent },
-  { path: 'story', component: StoryComponent },
-  { path: 'room', component: RoomComponent },
-  { path: 'reservation', component: ReservationComponent },
-  { path: 'manager', component: ManagerComponent },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'food', component: FoodComponent },
+    { path: 'flat-bench', component: FlatBenchComponent },
+    { path: 'story', component: StoryComponent },
+    { path: 'room', component: RoomComponent },
+    { path: 'reservation', component: ReservationComponent },
+    { path: 'manager', component: ManagerComponent },
+    { path: 'link-tree', component: LinkTreeComponent },
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, {
-      useHash: true,
-      scrollPositionRestoration: 'top',
-    }),
-  ],
-  exports: [RouterModule],
+    imports: [
+        RouterModule.forRoot(routes, {
+            useHash: true,
+            scrollPositionRestoration: 'top',
+        }),
+    ],
+    exports: [RouterModule],
 })
 export class AppRoutingModule {}
---
+++ src/app/link-tree/link-tree.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'link-tree',
+    templateUrl: './link-tree.component.html',
+    styleUrls: ['./link-tree.component.scss'],
+})
+export class LinkTreeComponent {
+    constructor() {}
+}
