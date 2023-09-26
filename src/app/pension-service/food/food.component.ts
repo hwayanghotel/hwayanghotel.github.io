9a284db75bd51ba66078605f1c05a4d24ac10cec
holywaterBetter <holywater.better@gmail.com>
2023-09-26 09:47:03 +0900

    feat(#27): Component Food 업데이트

--- src/app/pension-service/food/food.component.ts
+++ src/app/pension-service/food/food.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { Price } from 'src/assets/price';
 
 @Component({
-  selector: 'food',
-  templateUrl: './food.component.html',
-  styleUrls: ['./food.component.scss', '../pension-service.scss'],
+    selector: 'food',
+    templateUrl: './food.component.html',
+    styleUrls: ['./food.component.scss', '../pension-service.scss'],
 })
 export class FoodComponent {
-  Price = Price;
+    Price = Price;
 }
