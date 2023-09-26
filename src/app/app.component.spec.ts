d47a4d0ff97adbdad487a6dd3545de654f1db456
holywaterBetter <holywater.better@gmail.com>
2023-09-26 09:44:32 +0900

    feat(#27): AppComponent 내 header 등 삭제

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,25 +3,25 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(() =>
-    TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [AppComponent],
-    })
-  );
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'hwayanghotel app is running!'
+    beforeEach(() =>
+        TestBed.configureTestingModule({
+            imports: [RouterTestingModule],
+            declarations: [AppComponent],
+        })
     );
-  });
+
+    it('should create the app', () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.componentInstance;
+        expect(app).toBeTruthy();
+    });
+
+    it('should render title', () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        fixture.detectChanges();
+        const compiled = fixture.nativeElement as HTMLElement;
+        expect(compiled.querySelector('.content span')?.textContent).toContain(
+            'hwayanghotel app is running!'
+        );
+    });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,5 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-    get isManagerPage(): boolean {
-        return window.location.href.includes('manager');
-    }
-}
+export class AppComponent {}
